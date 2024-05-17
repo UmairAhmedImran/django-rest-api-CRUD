a492decf44a82a69644d91036d14e37d0c3839d0
UmairAhmedImran <umairahmedimran@gmail.com>
2024-05-17 21:59:34 +0500

    Creating the serializer file and adding a simple generic view

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import BlogPost 
+from .serializers import BlogPostSerializer
 
 # Create your views here.
+
+class BlogPostListCreate(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
