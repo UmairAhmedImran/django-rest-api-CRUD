9d104a998afbf48148450db10d24ff5be4d8e139
UmairAhmedImran <umairahmedimran@gmail.com>
2024-05-17 21:49:20 +0500

    creating the project and an app with name api

---
+++ mysite/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
